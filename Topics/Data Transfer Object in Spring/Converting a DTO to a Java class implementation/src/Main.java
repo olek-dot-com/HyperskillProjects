6bea459a17e41d3db87b458e200b37429cf459e4
olek-dot-com <120317212+olek-dot-com@users.noreply.github.com>
2023-10-25 16:32:20 +0200

    Delete Topics directory

--- Topics/Data Transfer Object in Spring/Converting a DTO to a Java class implementation/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.time.Month;
-
-class Solution {    
-    Product convertProductDTOToProduct(ProductDTO dto) {
-        Product product = new Product(dto.getId(), dto.getModel(), dto.getPrice(), LocalDate.of(2023, Month.JANUARY, 15), "SuperVendor");
-    return product;
-    }
-}
